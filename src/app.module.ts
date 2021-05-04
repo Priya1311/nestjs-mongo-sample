9a9a6ee87d2674d46f43e3b6dccc664a657d483c
Priya Gupta <priyagupta.90@gmail.com>
2021-05-04 18:16:26 +0530

    Connect to Mongo DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:admin@123@cluster0.mgpqu.mongodb.net/Courses?retryWrites=true&w=majority', { useNewUrlParser: true })
+  ,CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
