425c67bcae0f7a4a12dfe2f532a2eaba328109c5
Priya Gupta <priyagupta.90@gmail.com>
2021-05-04 18:19:27 +0530

    Import mongoose in module

--- src/courses/courses.mock.ts
+++ src/courses/courses.mock.ts
@@ -4,13 +4,13 @@
  * File Created: Tuesday, 4th May 2021 4:31:17 pm
  * Author: Priya Gupta (priya.gupta@mutualmobile.com)
  * -----
- * Last Modified: Tuesday, 4th May 2021 4:31:21 pm
+ * Last Modified: Tuesday, 4th May 2021 5:45:46 pm
  * Modified By: Priya Gupta (priya.gupta@mutualmobile.com)
  * -----
  * Copyright 2020 - 2021 Mutual Mobile, Mutual Mobile
  */
 export const COURSES = [
-    { id: 1, title: 'NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)', description: "Master Node JS, build REST APIs with Node.js, GraphQL APIs, add Authentication, use MongoDB, SQL & much more!", author: 'Maximilian Schwarzmüller', url: 'https://codingthesmartway.com/courses/nodejs-complete-guide/' },
-    { id: 2, title: 'The Complete Web Developer in 2020: Zero to Mastery', description: "Learn to code and become a Web Developer in 2020 with HTML, CSS, Javascript, React, Node.js, Machine Learning & more!", author: 'Andrei Neagoie', url: 'https://codingthesmartway.com/courses/web-developer-2018/' },
-    { id: 3, title: 'Learn and Understand NodeJS', description: "Dive deep under the hood of NodeJS. Learn V8, Express, the MEAN stack, core Javascript concepts, and more.", author: 'Anthony Alicea', url: 'https://codingthesmartway.com/courses/learn-understand-nodejs/' },
+    { id: 1, title: 'NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)', description: "Master Node JS, build REST APIs with Node.js, GraphQL APIs, add Authentication, use MongoDB, SQL & much more!", author: 'Maximilian Schwarzmüller', url: 'https://codingthesmartway.com/courses/nodejs-complete-guide/', created_at: '' },
+    { id: 2, title: 'The Complete Web Developer in 2020: Zero to Mastery', description: "Learn to code and become a Web Developer in 2020 with HTML, CSS, Javascript, React, Node.js, Machine Learning & more!", author: 'Andrei Neagoie', url: 'https://codingthesmartway.com/courses/web-developer-2018/', created_at: '' },
+    { id: 3, title: 'Learn and Understand NodeJS', description: "Dive deep under the hood of NodeJS. Learn V8, Express, the MEAN stack, core Javascript concepts, and more.", author: 'Anthony Alicea', url: 'https://codingthesmartway.com/courses/learn-understand-nodejs/', created_at: '' },
 ];
--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CoursesSchema } from './courses.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'Courses', schema: CoursesSchema }])],
   controllers: [CoursesController],
   providers: [CoursesService]
 })
